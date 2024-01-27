128c42520b6b12f4c15a4ec77ee7193db044ec13
Thanu Poptiphueng <tpcrasite@gmail.com>
2024-01-27 13:09:36 +0700

    Add CA Tidbit

---
+++ X509/README.md
@@ -0,0 +1,5 @@
+1. Read `create-ca.sh`
+   this script creates a CA and a server certificate and also client certificate
+   - `-extensions` specify the field to read the configuration from
+   - `-extfile extraInfo.txt` specify the file
+2. View `extraInfo.txt` to see how to add SANs to the server certificate
---
+++ X509/create-ca.sh
@@ -0,0 +1,18 @@
+## Create a private key for CA
+openssl genpkey -out ca-key -algorithm Ed25519 #Ed25519 key
+## Create a self-signed certificate for CA
+openssl req -new -x509 -nodes -days 3650 -key ca-key -out ca-cert.crt -subj "/O=Crasite, Inc./CN=Crasite Root CA"
+
+## Create a key for server
+openssl genrsa -out server-key 2048
+openssl req -new -key server-key -out server.csr -subj "/O=Crasite, Inc./CN=crasite.com"
+## use this to create a key with request
+#openssl req -newkey rsa:2048 -nodes -days 365  -keyout server-key.pem  -out server-req.pem -subj "/O=Crasite, Inc./CN=crasite.com"
+## Signing Certificate
+openssl x509 -req -days 365 \
+	-in server.csr \
+	-out server.crt \
+	-CA ca-cert.crt \
+	-CAkey ca-key \
+	-extensions SAN \
+	-extfile extraInfo.txt
