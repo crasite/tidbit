bcd0ab8dea29a3d8e0f5a9657af8a7b9511a5602
Thanu Poptiphueng <tpcrasite@gmail.com>
2024-02-11 13:17:15 +0700

    basic json return list

--- java-spring/src/main/java/com/example/demo/DemoApplication.java
+++ java-spring/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,5 +21,11 @@ public class DemoApplication {
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
     a += 1;
     return String.format("Hello %s! (%s)", name, a);
+  }
+
+  @GetMapping("/world")
+  public List<String> world(@RequestParam(value = "name", defaultValue = "World") String name) {
+    a += 1;
+    return List.of(String.format("Hello %s! (%s)", name, a), "hi");
   }
 }
